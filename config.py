c4cd15a9a32397b76fa74fdd2445d59aef407d0e
MLZ-BOTZ <125456401+MLZ-BOTZ@users.noreply.github.com>
2023-02-18 11:01:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,4 @@ START_PIC = os.environ.get("START_PIC", "")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1833209093 5845960615 5596825598').split()]
 
-
-
 PORT = os.environ.get('PORT', '8080')
